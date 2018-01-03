f0b0a9f3d869eebe8cf06614be6d10bc60356dab
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-01-03 01:45:10 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+
+
+def turn_count(board)
+  counter = 0
+  board.each do |playerinput|
+    if playerinput == "X" || "O"
+      counter += 1
+    end
+  end
+end
+
+
+def current_player(board)
+  playerinput == turn_count(board) + 1
+    if playerinput.even? == true
+      return "X"
+    elsif playerinput.even? == true
+      return "O"
+    end
+end
