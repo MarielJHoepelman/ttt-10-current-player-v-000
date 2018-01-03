58a76a0d3d70ec8166dfb725988b4010bbb975f2
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-01-03 01:49:06 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@
 def turn_count(board)
   counter = 0
   board.each do |playerinput|
-    if playerinput == "X" || "O"
+    if playerinput == "X" || playerinput == "O"
       counter += 1
     end
   end
